fba04d62953fe733a2e5c6816ba3c5b8bd5ea040
Kirill Timofeev <sushi-maker@yandex.ru>
2023-10-12 09:50:41 +0300

    Home work lesson 3

--- src/Human.java
+++ src/Human.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Human {
+public class Human {
+    String name;
+    String surname;
+    String secondname;
+    String date;
+    String phone;
+    String male;
+
+    public Human(String surname, String name, String secondname, String date, String phone, String male) {
+        this.name = name;
+        this.surname = surname;
+        this.secondname = secondname;
+        this.date = date;
+        this.phone = phone;
+        this.male = male;
+    }
+
+    @Override
+    public String toString() {
+        return  String.format("<%s><%s><%s><%s><%s><%s>",surname, name, secondname, date, phone, male);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,66 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        //ввод данных
+
+        Scanner inputString = new Scanner(System.in);
+        System.out.println("Введите данные пользователя"); // Тимофеев Кирилл Владиславович 27.04.1990 89215744948 м
+        String data = inputString.nextLine(); //Тимофеев Иван Владиславович 27.04.1990 89215744948 м
+        System.out.println(data);//Семенов Иван Владиславович 27.04.1990 89215744948 м
+
+        // массив создаем  из данных
+        String[] parse = data.split(" ");
+        int lenght = parse.length;
+        System.out.println(lenght);
+
+        //Проверяем длинну масиива и  называем переменные
+        if (lenght != 6) {
+            throw new RuntimeException("Введены данные не корреткно");
+
+        }
+            String surname = parse[0];
+            String name = parse[1];
+            String secondname = parse[2];
+            String date = parse[3];
+            String phone = parse[4];
+            String male = parse[5];
+
+
+        //Проверка  полей на корректность ввода
+
+        if (date.length() != 10 ) {
+            throw new RuntimeException("Не корректно введена дата");
+        }
+        if ( phone.length() != 11) {
+            throw new RuntimeException("Не корректно введен телефон");
+        }
+        if ( male.length() != 1) {
+            throw new RuntimeException("Не корректно введен пол");
+        }
+        // создаем объект Human
+        Human man = new Human(surname, name, secondname, date, phone, male);
+        String filename = man.surname + ".txt";
+
+        //создаем файл
+
+
+        try(FileWriter file = new FileWriter(filename, true))
+        {
+
+            file.write(man.toString());
+            file.write("\n");
+
+
+
+
+        }
+        catch (IOException e){
+            throw  new  RuntimeException("Ошибка чтения записи файла");
+        }
+
     }
 }
